2aee6bf443b5f740bee878fcb3e0a48a54d2da3f
marcushoang888 <marcushoang.888@gmail.com>
2024-08-15 16:48:06 +0700

    change api2

--- components/news/News.js
+++ components/news/News.js
@@ -1,33 +1,41 @@
 let news = document.querySelector(".news");
 
-function getNumber(number) {
-    fetch(
-        `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=b525d9255c0c463ab0fef771579f365a&page=${number}&pageSize=5` 
+const url =
+    "https://api.worldnewsapi.com/search-news?max-sentiment=-0.4&news-sources=https%3A%2F%2Fwww.huffingtonpost.co.uk&language=en";
+const apiKey = "387544140b0646829f150a7e7aa81971";
+
+fetch(url, {
+    method: "GET",
+    headers: {
+        "x-api-key": apiKey,
+    },
+})
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+    })
+    .then((data) => {
+        news.innerHTML = ``;
+        let articles = data.news;
+        console.log(articles);
         
-    )
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error("Network response was not ok");
-            }
-            return response.json();
-        })
-        .then((data) => {
-            news.innerHTML = ``;
-            let articles = data.articles;
-            articles.forEach((article) => {
-                news.innerHTML += `
+        articles.forEach((article) => {
+            news.innerHTML += `
             <div class="articleComponent">
-              <img class="articleImg" src="${article.urlToImage}" alt="" />
+              <img class="articleImg" src="${article.image}" alt="" />
               <div class="articleContent">
 
                 <h3 class="articleTitle">${article.title}</h3>
-                <p class="articleDesc">${article.content}</p>
-                <a class ="articleLink btnSubmit" href="${article.url}" target="_blank">Read more</a>
+                <a class ="articleLink btnSubmit" href="${article.summary}" target="_blank">Read more</a>
               </div>
               </div>
             `;
-            });
         });
-}
+    })
+    .catch((error) =>
+        console.error("There was a problem with the fetch operation:", error)
+    );
 
-getNumber(1);
+// getNumber(1);
